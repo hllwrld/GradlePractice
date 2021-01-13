06f9eaa6bc522d36b0e22df5386564e539e957d1
hllwrld <@tdGF:Z4"4Kctu=>
2021-01-13 18:38:53 +0800

    plugin
    Signed-off-by: hllwrld <@tdGF:Z4"4Kctu=>

---
+++ buildSrc/src/main/groovy/com/hllwrld/router/gradle/RouterPlugin.groovy
@@ -0,0 +1,12 @@
+package com.hllwrld.router.gradle
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class RouterPlugin implements Plugin<Project> {
+
+
+    void apply(Project project) {
+        println("hllwrld plugin success!!")
+    }
+
+}
